c3feb0919962ff9b08ed53fc22ce6eca907d5a74
Pusher <crystian@gmail.com>
2019-03-18 01:53:03 -0300

    req5

--- app/index.js
+++ app/index.js
@@ -1,1 +1,24 @@
-console.log('index.js loaded');
+const body = document.getElementsByTagName('body')[0];
+
+function createTable(alto, ancho, altoL, anchoL) {
+	let r = '';
+	for (let i = 0; i < alto + (altoL * 2); i++) {
+		r += '<tr>';
+		for (let j = 0; j < ancho + (anchoL * 2); j++) {
+			let limit =
+				(i < altoL || i >= (alto + altoL))
+					||
+				(j < anchoL || j >= (ancho + anchoL))
+					? 'limit' : 'normal';
+			r += `<td id="cell${i}-${j}" class="${limit}">${i}-${j}</td>`;
+		}
+		r += '</tr>';
+	}
+
+	return r;
+}
+
+let table = document.createElement('table');
+table.innerHTML = createTable(5, 5, 1, 1);
+
+body.prepend(table);
